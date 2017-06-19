521fd8f96d20b2efcc321957491934c6237a1404
Trenton Mcleod <Boanerges@Trentons-MacBook-Pro.local>
2017-06-18 21:32:01 -0400

    change data config

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Main = require("./components/main");
+//var routes = require("./config/routes");
+
+ReactDOM.render(<Main />, document.getElementById("app"));
